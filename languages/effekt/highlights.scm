46b4de435e5b33de5da09431605c04ccf279766a
Leon Fuss <leon.fuss@icloud.com>
2024-08-02 16:45:25 +0200

    bumped tree-sitter version and adapted highlights

--- languages/effekt/highlights.scm
+++ languages/effekt/highlights.scm
@@ -14,34 +14,63 @@
   "var"
   "try"
   "with"
+  "resume"
 ] @keyword
 
+"def" @keyword.function
+
+[
+ "("
+ ")"
+ "["
+ "]"
+ "{"
+ "}"
+]  @punctuation.bracket
+
+[
+ "."
+ ","
+] @punctuation.delimiter
+
+"import" @include
+
 (comment) @comment
-(number) @number
-(string) @string
-(boolean) @boolean
-
-(parameter_type (type_identifier) @type)
-(parameter_type (generic_identifier) @generic)
-
-(constructor (type_identifier) @type.constructor)
-(constructor_pattern (type_identifier) @type.constructor)
-
-; enum highlight is not supported
-; (type (parameter_type (type_identifier) @enum.name))
-; (type_member (type_identifier) @enum.member)
-
-(type (parameter_type (type_identifier) @type.name))
-(type_member (type_identifier) @type.member)
 
 
-(function (identifier) @function.name)
-(function (parameters(parameter (identifier) @parameter.name)))
+; literals
+[
+(integer_literal)
+(float_literal)
+] @number
+(string_literal) @string
+(boolean_literal) @boolean
 
-(interface_method (identifier) @function.name)
-(interface_method (parameters(parameter (identifier) @parameter.name)))
+(hole) @hole
 
-(function_call (identifier) @function.call.name)
+; functions
+(call_expression function: (identifier) @function.name)
+(field_declaration (field_identifier) @property)
 
-(call_chain (identifier) @variable.name)
-(call_chain (pre_arg_function_call (identifier) @function.call.name))
+(function_item  name: (identifier) @function.name)
+
+
+;(parameters (parameter (pattern (identifier) @parameter)))
+
+; types
+(type_identifier) @type.name
+(primitive_type) @type.buildin
+(generic_type (type_arguments (type_identifier) @type.generic))
+
+
+; effects
+(effect_item name: (type_identifier) @effect)
+(effect_alias [
+    (primitive_type)
+    (type_identifier)
+    ] @effect)
+
+(interface_item
+    body: (declaration_list
+        (function_item
+            name: (identifier) @effect)))
